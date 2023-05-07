a27743473f69c268996ac3ea8a6c4b376542a98c
sujinpark6774 <psujin6774@naver.com>
2023-05-07 15:19:37 +0900

    Linked List append, get 구현

---
+++ 1_List/linkedList_3_insert.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, value = 0, next = None):
+        self.value = value
+        self.next = next
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while(current.next):
+                current = current.next
+            current.next = new_node
+
+    def get(self, idx):
+        current = self.head
+        for _ in range(idx):
+            current = current.next
+        return current.value
+    
+    def insert(self, idx, value):
+        pass 
+        # 코드 짜보기
+
+
+
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.insert(idx = 2, value = 9 )
